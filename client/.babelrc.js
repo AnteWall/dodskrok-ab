691d6eb7ae1dfda915dff0d8ecd7bce765f1f599
Ante Wall <antewall@gmail.com>
2019-11-06 21:51:33 +0100

    More setup for client

---
+++ client/.babelrc.js
@@ -0,0 +1,26 @@
+const plugins = [
+  [
+    'babel-plugin-import',
+    {
+      libraryName: '@material-ui/core',
+      // Use "'libraryDirectory': ''," if your bundler does not support ES modules
+      libraryDirectory: 'esm',
+      camel2DashComponentName: false
+    },
+    'core'
+  ],
+  [
+    'babel-plugin-import',
+    {
+      libraryName: '@material-ui/icons',
+      // Use "'libraryDirectory': ''," if your bundler does not support ES modules
+      libraryDirectory: 'esm',
+      camel2DashComponentName: false
+    },
+    'icons'
+  ]
+];
+
+const presets = ['react-app'];
+
+module.exports = { plugins, presets };
